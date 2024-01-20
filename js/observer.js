479e43b4ff4b14467f9d564b69c2ea6949579b8a
LesterR <32542078+lhes23@users.noreply.github.com>
2024-01-20 19:24:05 +0800

    Update observer.js

--- js/observer.js
+++ js/observer.js
@@ -4,7 +4,7 @@
             const section1 = document.querySelector("#section-2-desktop")
             
             const whiteTouchBtn = document.getElementById("get-in-touch-white")
-            var whiteTouchBtnChildren = whiteTouchBtn.querySelector("a");
+            const whiteTouchBtnChildren = whiteTouchBtn.querySelector("a");
   
             const navElementWhite = document.getElementById("nav-element")
             const navElementWhiteChildren = navElementWhite.querySelector("svg")
@@ -23,7 +23,7 @@
             const navMobileWhite= document.getElementById("nav-element-mobile-white")
             
             const sectionOneOptions = {
-                rootMargin: "-100px 0px 0px 0px"
+                rootMargin: "-100px 0px 0px 0px",
             };
 
 
@@ -35,34 +35,13 @@
                         
                         navElementWhiteChildren.style.fill = "#c4d018"
                         logoWhiteImg.src = "https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img,w_800,h_166/https://butlersigns.uk/wp-content/uploads/2022/06/Butler-Signs-Light-Background-Logo.png"
-                        
-                        
-                        // navElementWhite.style.display ="none"
-                        logoWhite.style.display = "block"
-                        navElementGreen.style.display = "block"
-                        blackTouchBtn.style.display = "block"
-                        blackTouchBtn.style.width = "100%"
-                        blackTouchBtn.style.top = null
-                        
-                        logoBlack.style.display = "block"
-                        console.log("not")
+
                     } else {
-                        // whiteTouchBtn.style.display = "block"
                         whiteTouchBtnChildren.style.color="white"
                         whiteTouchBtnChildren.style.borderColor="white"
                         navElementWhiteChildren.style.fill = "#fff"
                         logoWhiteImg.src = "https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img,w_800,h_165/https://butlersigns.uk/wp-content/uploads/2022/06/Butler-logo-white-2-1024x211.png"
-                        
-                        
-                        
-                        logoWhite.style.display = "block"
-                        logoWhite.style.width = "100%"
-                        logoWhite.style.top = null
-                        
-                        navElementGreen.style.display = "block"
-                        blackTouchBtn.style.display = "block"
-                        logoBlack.style.display = "block"
-                        console.log("yes")
+
                     }
                 })
 
