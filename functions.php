9fd842eaecc828697a60c0aa3b94d976fcf6725f
LesterR <32542078+lhes23@users.noreply.github.com>
2024-01-20 18:49:50 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -35,62 +35,7 @@ function hello_elementor_child_scripts_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
 
-function ti_custom_javascript() {
-    ?>
-        <script>
-          document.addEventListener("DOMContentLoaded", function () {
-
-            const whiteTouchBtn = document.getElementById("get-in-touch-white")
-            const navElementWhite = document.getElementById("nav-element")
-            
-            const blackTouchBtn = document.getElementById("get-in-touch-black")
-            const navElementGreen = document.getElementById("nav-element-green")
-            
-            const logoWhite = document.getElementById("homepage-logo-white2")
-            const logoBlack = document.getElementById("homepage-logo-black")
-            
-            const section1 = document.querySelector("#section-2-desktop")
-            
-            const sectionOneOptions = {
-  rootMargin: "-80px 0px 0px 0px"
-};
-
-            
-
-            const sectionObserver = new IntersectionObserver(function (entries, sectionObserver) {
-                entries.forEach(entry => {
-                    if (!entry.isIntersecting) {
-                        whiteTouchBtn.style.display = "none"
-                        navElementWhite.style.display ="none"
-                        logoWhite.style.display = "none"
-                        navElementGreen.style.display = "block"
-                        blackTouchBtn.style.display = "block"
-                        blackTouchBtn.style.width = "100%"
-                        blackTouchBtn.style.top = null
-                        
-                        logoBlack.style.display = "block"
-                        console.log("not")
-                    } else {
-                        whiteTouchBtn.style.display = "block"
-                        navElementWhite.style.display ="block"
-                        logoWhite.style.display = "block"
-                        logoWhite.style.width = "100%"
-                        logoWhite.style.top = null
-                        
-                        navElementGreen.style.display = "none"
-                        blackTouchBtn.style.display = "none"
-                        logoBlack.style.display = "none"
-                        console.log("yes")
-                    }
-                })
-
-            },sectionOneOptions)
-
-            sectionObserver.observe(section1)
-
-
-        });
-        </script>
-    <?php
+function my_custom_scripts() {
+    wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/js/observer.js', array( 'jquery' ),'',true );
 }
-add_action('wp_head', 'ti_custom_javascript');
+add_action( 'wp_enqueue_scripts', 'my_custom_scripts' );
